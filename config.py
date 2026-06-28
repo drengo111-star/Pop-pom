c7d5c74c85c94efe64fa33d522be69534d8dff7a
drengo111-star <drengo111@gmail.com>
2026-06-28 18:27:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# Bot Configuration - Environment variables for Render
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8609436617:AAFASxbiEFDFLDtUi722p_41txRE-sEYJq8")
+UPI_ID = os.environ.get("UPI_ID", "grouthzone@okicici")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003758981914"))
